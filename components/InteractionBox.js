5efea95d0f530df0067d3f5e7b02b44cd268c21b
Habib <h.hourany@gmail.com>
2024-06-20 11:11:35 +0200

    Style Interaction Box

--- components/InteractionBox.js
+++ components/InteractionBox.js
@@ -1,30 +1,52 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { FontAwesome6 } from "@expo/vector-icons";
 
 export function InteractionBox({ interaction }) {
   return (
-    <View style={styles.interactionBox}>
+    <View style={[styles.interactionBox, styles[interaction.severity]]}>
       <View style={styles.interactionHeader}>
-        <View style={styles.drugs}>
-          <Text>{interaction.drug1}</Text>
-          <Text>{interaction.drug2}</Text>
-        </View>
-        <View style={styles.severity}>
-          <Text>{interaction.severity}</Text>
-        </View>
+        <Text style={styles.drugName}>{interaction.drug1}</Text>
+        <FontAwesome6
+          name="arrows-left-right"
+          size={20}
+          style={styles.interactionArrow}
+        />
+        <Text style={[styles.drugName, { textAlign: "right" }]}>
+          {interaction.drug2}
+        </Text>
       </View>
       <View style={styles.interactionDescription}>
-        <Text>{interaction.description}</Text>
+        <Text style={{ fontStyle: "italic" }}>{interaction.description}</Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  interactionBox: { backgroundColor: "#F6F6F6", padding: 10, borderRadius: 10 },
-  interactionHeader: { flexDirection: "row" },
-  drugs: {},
-  severity: {},
+  interactionBox: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    borderWidth: 2,
 
-  interactionDescription: {},
+    padding: 10,
+    gap: 10,
+  },
+  minor: { borderColor: "limegreen" },
+  moderate: { borderColor: "orange" },
+  major: { borderColor: "red" },
+
+  interactionHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  drugName: { flex: 1, flexWrap: "wrap" },
+  interactionArrow: { color: "#A09DAE" },
+
+  interactionDescription: {
+    borderTopColor: "#F6E7E5",
+    borderTopWidth: 1,
+    paddingTop: 5,
+  },
 });
